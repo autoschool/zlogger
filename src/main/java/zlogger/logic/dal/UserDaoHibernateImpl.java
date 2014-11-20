addcfb34714cf20829f307f8a9f9d64d9b1b17f8
Alexander Pisarkov <alexwyrm@gmail.com>
2014-11-21 01:33:20 +0300

    Reduce technical debt

--- src/main/java/zlogger/logic/dal/UserDaoHibernateImpl.java
+++ src/main/java/zlogger/logic/dal/UserDaoHibernateImpl.java
@@ -1,7 +1,5 @@
 package zlogger.logic.dal;
 
-import org.hibernate.HibernateException;
-import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
@@ -16,38 +14,30 @@ import java.util.List;
 public class UserDaoHibernateImpl implements UserDao {
 
     @Autowired
-    private SessionFactory mySessionFactory;
-
-    private Session getCurrentSession() {
-        Session session;
-        try {
-            session = mySessionFactory.getCurrentSession();
-        } catch (HibernateException he) {
-            session = mySessionFactory.openSession();
-        }
-        return session;
-    }
+    private SessionFactory sessionFactory;
 
     @Override
     public List<User> listUsers() {
-        return getCurrentSession().createCriteria(User.class).list();
+        return sessionFactory.getCurrentSession()
+                .createCriteria(User.class).list();
     }
 
     @Override
     public User getUserById(Long id) {
-        User user = (User) getCurrentSession().load(User.class, id);
-        return user;
+        return (User) sessionFactory.openSession()
+                .load(User.class, id);
     }
 
     @Override
     public void deleteUserById(Long id) {
-        User user = (User) getCurrentSession().load(User.class, id);
-        getCurrentSession().delete(user);
+        User user = (User) sessionFactory.getCurrentSession()
+                .load(User.class, id);
+        sessionFactory.getCurrentSession().delete(user);
     }
 
     @Override
     public Long createUser(User user) {
-        getCurrentSession().save(user);
+        sessionFactory.getCurrentSession().save(user);
         return user.getUserId();
     }
 
@@ -55,12 +45,13 @@ public class UserDaoHibernateImpl implements UserDao {
     public Long updateUser(User user) {
         if (user.getUserId() != null) {
             User oldUser = getUserById(user.getUserId());
-            if (oldUser == null)
+            if (oldUser == null) {
                 return null;
+            }
 
             user.setLogin(oldUser.getLogin());
         }
-        getCurrentSession().merge(user);
+        sessionFactory.getCurrentSession().merge(user);
         return user.getUserId();
     }
 }
