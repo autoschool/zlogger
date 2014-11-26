d1074c114fc187c3b3a7d02ae32ef5e44fed25b8
Alexander Pisarkov <alexwyrm@gmail.com>
2014-11-26 10:31:36 +0300

    Fixed authentication
    Created test pages /user and /admin for testing authentication

--- src/main/resources/test_database.sql
+++ src/main/resources/test_database.sql
@@ -2,6 +2,7 @@ DROP TABLE IF EXISTS `authorities`;
 DROP TABLE IF EXISTS `commentaries`;
 DROP TABLE IF EXISTS `posts`;
 DROP TABLE IF EXISTS `walls`;
+DROP TABLE IF EXISTS `persistent_logins`;
 DROP TABLE IF EXISTS `users`;
 
 --
@@ -9,10 +10,9 @@ DROP TABLE IF EXISTS `users`;
 --
 
 CREATE TABLE `users` (
-  `username` varchar(50) NOT NULL,
-  `enabled` tinyint(1) NOT NULL,
-  `password` varchar(50) NOT NULL,
-  PRIMARY KEY (`username`)
+      `username` varchar_ignorecase(50) NOT NULL PRIMARY KEY,
+      `password` varchar_ignorecase(50) NOT NULL,
+      `enabled` boolean NOT NULL
 ) DEFAULT CHARSET=utf8;
 
 --
@@ -23,7 +23,7 @@ CREATE TABLE `walls` (
   `wall_id` bigint(20) NOT NULL AUTO_INCREMENT,
   `owner_name` varchar(50) NOT NULL,
   PRIMARY KEY (`wall_id`),
-  CONSTRAINT `FK_walls_owner_name` FOREIGN KEY (`owner_name`) REFERENCES `users` (`username`)
+  CONSTRAINT `fk_walls_owner_name` FOREIGN KEY (`owner_name`) REFERENCES `users` (`username`)
 ) AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
 --
@@ -31,11 +31,11 @@ CREATE TABLE `walls` (
 --
 
 CREATE TABLE `authorities` (
-  `authority` varchar(50) NOT NULL,
-  `username` varchar(50) NOT NULL,
-  PRIMARY KEY (`username`),
-  CONSTRAINT `FK_authorities_username` FOREIGN KEY (`username`) REFERENCES `users` (`username`)
-) DEFAULT CHARSET=utf8;
+      `username` varchar_ignorecase(50) NOT NULL,
+      `authority` varchar_ignorecase(50) NOT NULL,
+      CONSTRAINT `fk_authorities_users` FOREIGN KEY (`username`) REFERENCES `users` (`username`)
+)  DEFAULT CHARSET=utf8;
+CREATE UNIQUE index `ix_auth_username` ON authorities (username,authority);
 
 --
 -- Table structure for table `posts`
@@ -43,14 +43,14 @@ CREATE TABLE `authorities` (
 
 CREATE TABLE `posts` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `creation_date` timestamp NOT NULL,
-  `message` varchar(2000) NOT NULL,
   `title` varchar(255) DEFAULT NULL,
+  `message` varchar(2000) NOT NULL,
+  `creation_date` timestamp NOT NULL,
   `creator_name` varchar(50) NOT NULL,
   `wall_id` bigint(20) NOT NULL,
   PRIMARY KEY (`id`),
-  CONSTRAINT `FK_posts_creator_name` FOREIGN KEY (`creator_name`) REFERENCES `users` (`username`),
-  CONSTRAINT `FK_posts_wall_id` FOREIGN KEY (`wall_id`) REFERENCES `walls` (`wall_id`)
+  CONSTRAINT `fk_posts_creator_name` FOREIGN KEY (`creator_name`) REFERENCES `users` (`username`),
+  CONSTRAINT `fk_posts_wall_id` FOREIGN KEY (`wall_id`) REFERENCES `walls` (`wall_id`)
 ) AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
 --
@@ -59,29 +59,40 @@ CREATE TABLE `posts` (
 
 CREATE TABLE `commentaries` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `creation_date` timestamp NOT NULL,
   `message` varchar(2000) NOT NULL,
+  `creation_date` timestamp NOT NULL,
   `creator_name` varchar(50) NOT NULL,
   `post_id` bigint(20) NOT NULL,
   PRIMARY KEY (`id`),
-  CONSTRAINT `FK_commentaries_creator_name` FOREIGN KEY (`creator_name`) REFERENCES `users` (`username`),
-  CONSTRAINT `FK_commentaries_post_id` FOREIGN KEY (`post_id`) REFERENCES `posts` (`id`) ON DELETE CASCADE
+  CONSTRAINT `fk_commentaries_creator_name` FOREIGN KEY (`creator_name`) REFERENCES `users` (`username`),
+  CONSTRAINT `fk_commentaries_post_id` FOREIGN KEY (`post_id`) REFERENCES `posts` (`id`) ON DELETE CASCADE
 ) AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+---
+--- Table structure for table `persistent_logins`
+---
+
+CREATE TABLE `persistent_logins` (
+  `username` varchar(64) NOT NULL,
+  `series` varchar(64) PRIMARY KEY,
+  `token` varchar(64) NOT NULL,
+  `last_used` timestamp NOT NULL
+) DEFAULT CHARSET=utf8;
 
 --
 -- data
 --
 
-INSERT INTO `users` VALUES ('testAdmin', 1, 'password');
-INSERT INTO `users` VALUES ('testUser', 1, 'password');
+INSERT INTO `users` VALUES ('testAdmin', 'password', 1);
+INSERT INTO `users` VALUES ('testUser', 'password', 1);
 
-INSERT INTO `authorities` VALUES ('ROLE_ADMIN', 'testAdmin');
-INSERT INTO `authorities` VALUES ('ROLE_USER', 'testUser');
+INSERT INTO `authorities` VALUES ('testAdmin', 'ROLE_ADMIN');
+INSERT INTO `authorities` VALUES ('testUser', 'ROLE_USER');
 
 INSERT INTO `walls` VALUES (1,'testUser');
 
-INSERT INTO `posts` VALUES (1,'2014-11-24 15:06:36','test message 1','test title','testUser', 1);
-INSERT INTO `posts` VALUES (2,'2014-11-24 15:06:39','test message 2','test title','testAdmin', 1);
-INSERT INTO `posts` VALUES (3,'2014-11-24 15:06:40','test message 3','test title','testUser', 1);
+INSERT INTO `posts` VALUES (1,'test title','test message 1','2014-11-24 15:06:36','testUser', 1);
+INSERT INTO `posts` VALUES (2,'test title','test message 2','2014-11-24 15:06:39','testAdmin', 1);
+INSERT INTO `posts` VALUES (3,'test title','test message 3','2014-11-24 15:06:40','testUser', 1);
 
-INSERT INTO `commentaries` VALUES (1,'2014-11-24 15:06:40','test message 1','testAdmin', 1);
+INSERT INTO `commentaries` VALUES (1,'test message 1','2014-11-24 15:06:40','testAdmin', 1);
