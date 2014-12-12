f9d2bcac5982f5c3bd40e27efffa9a4b8173a3c3
RouR <rour@yandex.ru>
2014-12-12 15:57:05 +0300

    web - add commentary by user

--- src/main/java/zlogger/web/controllers/CommentariesController.java
+++ src/main/java/zlogger/web/controllers/CommentariesController.java
@@ -2,11 +2,15 @@ package zlogger.web.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import zlogger.logic.models.Commentary;
+import zlogger.logic.models.Post;
+import zlogger.logic.models.User;
 import zlogger.logic.services.CommentaryService;
 import zlogger.logic.services.PostService;
+import zlogger.logic.services.UserService;
 
 import javax.ws.rs.core.MediaType;
 import java.util.List;
@@ -19,6 +23,8 @@ public class CommentariesController {
     @Autowired
     CommentaryService commentaryService;
 
+    @Autowired
+    UserService userService;
 
     @RequestMapping(value = "/post/{id}/commentaries",
             method = RequestMethod.GET,
@@ -29,4 +35,13 @@ public class CommentariesController {
         return commentaryService.listForPost(postService.get(postId));
     }
 
+    @RequestMapping(value = "/post/{id}/addcomment",  method = RequestMethod.POST)
+    public String addComment(@PathVariable("id") long postId, Authentication authentication, @RequestParam String message ){
+        User user = userService.get(authentication.getName());
+        Post toPost =  postService.get(postId);
+        Commentary newCom = new Commentary();
+        newCom.setMessage(message);
+        commentaryService.add(newCom, toPost, user);
+        return "redirect:/post/"+postId;
+    }
 }
